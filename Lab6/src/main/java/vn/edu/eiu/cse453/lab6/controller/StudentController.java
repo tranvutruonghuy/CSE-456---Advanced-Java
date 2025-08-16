c38607ca22fda149157d34bc4ccc5426c8c61d94
tranvutruonghuy <huy.tranvutruong.dev157@gmail.com>
2025-08-16 23:55:00 +0700

    update

--- Lab6/src/main/java/vn/edu/eiu/cse453/lab6/controller/StudentController.java
+++ Lab6/src/main/java/vn/edu/eiu/cse453/lab6/controller/StudentController.java
@@ -21,19 +21,19 @@ public class StudentController {
     private final InitStudent initStudent;
 
     @Autowired
-    public StudentController(InitStudent initStudent){
+    public StudentController(InitStudent initStudent) {
         this.initStudent = initStudent;
     }
 
     //Hiển thị trang index
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index"; //index.html, không cần gõ html vì đã khai báo suffix
     }
 
     //Hiển thị danh sách sinh viên cho url localhost:8080/students
     @GetMapping("/students")
-    public String showStudents(Model model){
+    public String showStudents(Model model) {
         //Hàm được gọi khi user gõ url localhost:8080/students
         //Sau khi xử lý sẽ trả về một trang html
         //Nếu kèm theo data thì bỏ trong obj Model (attributeName, attributeValue)
@@ -43,15 +43,16 @@ public class StudentController {
         model.addAttribute("students", students);
         return "student-list"; //Chính là file student-list.html
     }
+
     //Xử lý trang localhost:8080/student/edit/id
-    @GetMapping("/student/edit/{id}")
-    public String showEditForm(Model model, @PathVariable int id){
+    @GetMapping("/students/edit/{id}")
+    public String showEditForm(Model model, @PathVariable int id) {
         //Các công việc cần làm:
         //Gửi kèm thông tin của sinh viên cần sửa
         //Trả về trang html có form edit student
 
-        for(Student foundStudent : initStudent.getStudentList()){
-            if(foundStudent.getId() == id){
+        for (Student foundStudent : initStudent.getStudentList()) {
+            if (foundStudent.getId() == id) {
                 //Gửi thông tin sinh viên cần sửa qua form
                 model.addAttribute("foundStudent", foundStudent);
                 break;
@@ -60,8 +61,13 @@ public class StudentController {
         return "student-edit-form";
     }
 
-    //Xử lý cho form edit sinh viên
-//    @PostMapping("/student/edit/result")
+
+//    //Xử lý cho form edit sinh viên
+//    //Phien ban 1: nhớ sửa đường dẫn action của form lại để sử dụng được phiên bản 1
+//    //Khi xử lý form
+//    //Sau khi xử lý và trả về trong html, nhưng url không thay đổi, vẫn là url của action form => nếu f5 thì sẽ gọi lại hàm xử lý này -> lỗi resumission forrm
+//    //Trong trường hợp thêm mới data, với key tự tăng thì sẽ bị duplicate dữ liệu
+//    @PostMapping("/students/edit/result")
 //    public String saveStudent(@RequestParam int id, @RequestParam String name, @RequestParam int yob, @RequestParam double gpa, Model model){
 //        //Lấy thông tin từ form
 //        //Hiển thị thông tin đủ lên trang result.html
@@ -73,12 +79,10 @@ public class StudentController {
 //        return "result"; //result.html
 //    }
 
-//    //Khi xử lý form
-//    //Sau khi xử lý và trả về trong html, nhưng url không thay đổi, vẫn là url của action form => nếu f5 thì sẽ gọi lại hàm xử lý này -> lỗi resumission forrm
-//    //Trong trường hợp thêm mới data, với key tự tăng thì sẽ bị duplicate dữ liệu
+
 //    //Phiên bản 2 sử dụng redirect để tránh lỗi
 //    //Xử lý cho form edit sinh viên
-//    @PostMapping("/student/edit")
+//    @PostMapping("/students/edit")
 //    public String saveStudent(@RequestParam int id, @RequestParam String name, @RequestParam int yob, @RequestParam double gpa, RedirectAttributes redireactAtt){
 //        //Lấy thông tin từ form
 //        //Hiển thị thông tin đủ lên trang result.html
@@ -88,36 +92,85 @@ public class StudentController {
 //        redireactAtt.addFlashAttribute("pyob", yob);
 //        redireactAtt.addFlashAttribute("pgpa", gpa);
 //        //Có flash để bảo mật thông tin (như kiểu gửi trong box)
-//        return "redirect:/student/edit/result";
+//        return "redirect:/students/edit/result";
 //        //chuyển hướng url thành localhost:8080/student/edit/result, không phải là trang result.html. Nên phải có hàm xử lý cho url này
 //        //Tại sao chỗ này không cần Model? Model sẽ được gửi kèm theo trong html cho thmeleaf dùng để mix. Mà ở đây không hề gửi trang html nào hết
 //    }
 //
+//    @GetMapping("/students/edit/result")
+//    public String showResult(Model model) {
+//        return "result"; //ở đây mới là trang result.html
+//        //Sẽ bị lỗi null vì không nhận được data từ model của bên @PostMapping saveStudent
+//        //Xử lý bằng cách khi redirect bên saveStudent, gửi kèm thêm gói hàng RedirectAttribute. Nghĩa là trong hàm này có 2 loại dữ liệu:
+//        // 1. Model chính của nó
+//        // 2. RedirectAttribute được gửi kèm theo từ bên saveStudent
+//
+//    }
 
-    //Phieen bản 3: lấy thông in đã sửa redirect sang trang Student-list
-    //Thực tế là: sau khi edit xong thì lưu db -> cp nhật lên students -> quay lại trang students
-    //Hiện tại chỉ làm: lấy thông tin cập nhật hiển thị lên trang students
-    @PostMapping("/student/edit")
-    public String saveStudent(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("yob") int yob, @RequestParam("gpa") double gpa, RedirectAttributes redireactAtt){
-        //Lấy thông tin từ form
-        //Hiển thị thông tin đủ lên trang result.html
-        redireactAtt.addFlashAttribute("pmsg", "Student has been updated");
-        redireactAtt.addFlashAttribute("pid", id);
-        redireactAtt.addFlashAttribute("pname", name);
-        redireactAtt.addFlashAttribute("pyob", yob);
-        redireactAtt.addFlashAttribute("pgpa", gpa);
-        //Có flash để bảo mật thông tin (như kiểu gửi trong box)
+
+//    //Phieen bản 3: lấy thông in đã sửa redirect sang trang Student-list
+//    //Thực tế là: sau khi edit xong thì lưu db -> cp nhật lên students -> quay lại trang students
+//    //Hiện tại chỉ làm: lấy thông tin cập nhật hiển thị lên trang students
+//    @PostMapping("/students/edit")
+//    public String saveStudent(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("yob") int yob, @RequestParam("gpa") double gpa, RedirectAttributes redireactAtt){
+//        //Lấy thông tin từ form
+//        //Hiển thị thông tin đủ lên trang result.html
+//        redireactAtt.addFlashAttribute("pmsg", "Student has been updated");
+//        redireactAtt.addFlashAttribute("pid", id);
+//        redireactAtt.addFlashAttribute("pname", name);
+//        redireactAtt.addFlashAttribute("pyob", yob);
+//        redireactAtt.addFlashAttribute("pgpa", gpa);
+//        //Có flash để bảo mật thông tin (như kiểu gửi trong box)
+//        return "redirect:/students";
+//    }
+
+
+
+    //Phiên bản 4: làm bình thường sau khi edit redirect về trang student list
+    @PostMapping("/students/edit")
+    public String saveStudent(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("yob") int yob, @RequestParam("gpa") double gpa){
+        //Lấy thông tin từ form, cập nhật cho student rồi redirect về students
+        for(Student student : initStudent.getStudentList()){
+            if(student.getId() == id){
+                student.setId(id);
+                student.setName(name);
+                student.setYob(yob);
+                student.setGpa(gpa);
+                break;
+            }
+        }
         return "redirect:/students";
-        //chuyển hướng url thành localhost:8080/student/edit/result, không phải là trang result.html. Nên phải có hàm xử lý cho url này
-        //Tại sao chỗ này không cần Model? Model sẽ được gửi kèm theo trong html cho thmeleaf dùng để mix. Mà ở đây không hề gửi trang html nào hết
     }
-    @GetMapping("/student/edit/result")
-    public String showResult(Model model){
-        return "result"; //ở đây mới là trang result.html
-        //Sẽ bị lỗi null vì không nhận được data từ model của bên @PostMapping saveStudent
-        //Xử lý bằng cách khi redirect bên saveStudent, gửi kèm thêm gói hàng RedirectAttribute. Nghĩa là trong hàm này có 2 loại dữ liệu:
-        // 1. Model chính của nó
-        // 2. RedirectAttribute được gửi kèm theo từ bên saveStudent
 
+
+
+    //Add student
+    @GetMapping("/students/new")
+    public String showAddForm() {
+        return "student-add-form";
     }
+
+    @PostMapping("/students/new")
+    public String addStudent(@RequestParam int id ,@RequestParam String name, @RequestParam int yob, @RequestParam double gpa) {
+        Student student = new Student(id, name, yob, gpa);
+        initStudent.getStudentList().add(student);
+        return "redirect:/students";
+    }
+
+
+
+
+    //Delete
+    @GetMapping("/students/delete/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        for(Student student : initStudent.getStudentList()){
+            if(student.getId() == id){
+                initStudent.getStudentList().remove(student);
+                break;
+            }
+        }
+        return "redirect:/students";
+    }
+
+
 }
